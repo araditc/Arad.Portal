4b09da87260d929d829c6de4deb357a12d0134a9
azizi <azizi@svn-itc.org>
2022-08-14 11:13:00 +0430

    a bug in promotion addEdit has fixed

--- Arad.Portal.UI.Shop/Controllers/ContentController.cs
+++ Arad.Portal.UI.Shop/Controllers/ContentController.cs
@@ -1,4 +1,4 @@
-﻿using Arad.Portal.DataLayer.Contracts.General.Content;
+﻿ using Arad.Portal.DataLayer.Contracts.General.Content;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
